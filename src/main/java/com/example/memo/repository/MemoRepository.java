ccf9a59ffadd7e7d55da78c3dd71bcc56ca89ff1
yonghyun0325 <dldydgus95426@gmail.com>
2026-06-26 12:09:24 +0900

    Spring boot의 JPA

--- src/main/java/com/example/memo/repository/MemoRepository.java
+++ src/main/java/com/example/memo/repository/MemoRepository.java
@@ -3,11 +3,14 @@ package com.example.memo.repository;
 import com.example.memo.dto.MemoRequestDto;
 import com.example.memo.dto.MemoResponseDto;
 import com.example.memo.entity.Memo;
+import jakarta.persistence.EntityManager;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -86,5 +89,15 @@ public class MemoRepository {
                 return null;
             }
         }, id);
+    }
+
+    @Transactional
+    public Memo createMemo(@NonNull EntityManager em) {
+        Memo memo = em.find(Memo.class, 1);
+        memo.setUsername("Robbert");
+        memo.setContents("@Transactional 전파 테스트 중! 2");
+
+        System.out.println("createMemo 메서드 종료");
+        return memo;
     }
 }
